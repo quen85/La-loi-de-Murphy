006bb2c900fc56fd9ebbd7268b19b831c7a73fb8
Héma Lambert <hema.lambert@opsone.net>
2017-10-27 16:31:03 +0200

    Fix Game Over

--- js/states/GameOver.js
+++ js/states/GameOver.js
@@ -9,7 +9,7 @@ LoiDeMurphy.GameOver.prototype = {
         this.splash.scale.setTo(0.2,0.2);
         var textGameOver = this.game.add.sprite(500,330,"texteGameOver");
         textGameOver.anchor.setTo(0.5);
-        textGameOver.scale.setTo(0.4,0.4);
+        textGameOver.scale.setTo(1, 1);
         var playButton = this.game.add.button(500,430,"replay-bouton",this.playTheGame,this);
         playButton.scale.setTo(0.1,0.1);
         playButton.anchor.setTo(0.5,0.5);
